f18b2c032de1f2658d177c098670542920bd1ff3
Kristina Hrynko <grnkkristina@gmail.com>
2025-05-07 13:54:23 +0300

    function search

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,12 +2,33 @@ import React, { useState } from 'react';
 import MovieCard from "./MovieCard";
 
 function MovieList({ movies }) {
+    const [searchItem, setSearchItem] = useState("");
+    const [filteredMovies, setFilteredMovies] = useState(movies);
+
+    const handleInputChange = (e) => {
+        const searchTerm = e.target.value;
+        setSearchItem(searchTerm)
+
+        const filteredItems = movies.filter((movie) =>
+            movie.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+
+        setFilteredMovies(filteredItems);
+    }
+
+
 
     return (
         <div className="movie-list-container">
+            <input
+                type="text"
+                value={searchItem}
+                onChange={handleInputChange}
+                placeholder="Find a movie..."
+            />
             <div className="movie-list">
                 {
-                    movies.map((movie) => (
+                    filteredMovies.map((movie) => (
                         <MovieCard
                             key={movie.id}
                             movie={movie}
